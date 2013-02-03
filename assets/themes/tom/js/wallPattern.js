17903112616773e266ae1a35ccf328530afa2cf8
Chris Lindgren <chris.a.lindgren@gmail.com>
2013-02-02 18:38:00 -0600

    new canvas

--- assets/themes/tom/js/wallPattern.js
+++ assets/themes/tom/js/wallPattern.js
@@ -1,93 +1,18 @@
-document.addEventListener('DOMContentLoaded',domloaded,false);
-function domloaded(){
+var c = document.createElement('canvas'),
+    ctx = c.getContext('2d'),
+    cw = c.width = 200,
+  	ch = c.height = 200,
+    rand = function(a,b){return ~~((Math.random()*(b-a+1))+a);}
 
-var wallPattern = {
-  
-  // Settings
-  spacingX: 35,
-  spacingY: 45,
-  offsetVariance: 10,
-  baseRadius: 55,
-  
-  // Other Globals
-  points: [],
-  canvas: null,
-  context: null,
-  
-  init: function() {
-    
-    this.canvas = document.getElementById( 'canvas' );
-    this.context = canvas.getContext( '2d' );
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
-    this.preparePoints();
-		this.createPattern();
-  },
-  
-  preparePoints: function() {
-    
-    var width, height, i, j, k, offsetX, offsetY;
-    var maxVariance = this.offsetVariance * 3;
-    
-    // Vertical spacing
-    for ( i = this.spacingY; i < this.canvas.height; i += this.spacingY ) {
-      
-      var pointSet = [];
-      
-      // Horizontal spacing
-      for(j = this.spacingX; j < this.canvas.width; j += this.spacingX ) {
-        
-        offsetX = Math.round((Math.random() * maxVariance) - this.offsetVariance);
-        offsetY = Math.round((Math.random() * maxVariance) - this.offsetVariance);
-        var offsetR = Math.round((Math.random() * maxVariance) - this.offsetVariance);
-        
-				pointSet.push( {x: j + offsetX, y: i + offsetY, radius: this.baseRadius + offsetR} )
-      }
-      
-      this.points.push( this.shuffleArray( pointSet ) );
-    } 
-  },
-
-  createPattern: function() {
-    
-		var i, j, k, currentPoints, currentPoint;
-    
-    for ( i = 0; i < this.points.length; i++ ) {
-      
-      currentPoints = this.points[i]; 
-      
-      for ( j = 0; j < currentPoints.length;  j++ ) {
-        	
-       	currentPoint = currentPoints[j];
-        for ( k = currentPoint.radius; k > 0; k-=3 ) {
-        	
-        	
-     	  this.context.beginPath();
-          this.context.arc(currentPoint.x, currentPoint.y, k ,0 , Math.PI*2, true);
-          this.context.closePath();
-          this.context.fillStyle='#ffffff';
-          this.context.strokeStyle='#9FCFE3';
-          this.context.fill();
-          this.context.stroke();   
-        }
-      }
+for(var w = 0; w < cw; w++){
+	for(var h = 0; h < ch; h++){
+    ctx.fillStyle = 'hsl(0, 0%, '+rand(90, 100)+'%)';
+		ctx.fillRect(w*8, h*4, 4, 4);
+    if(w % 6 === 0 && h % 18 === 0){
+      ctx.fillStyle = 'hsla(0, 0%, 0%, .11)';
+		  ctx.fillRect(w, h, 20, 1);
     }
-  },
- 
-  // Shuffle algorithm from: http://stackoverflow.com/questions/962802/is-it-correct-to-use-javascript-array-sort-method-for-shuffling
-  shuffleArray: function( array ) {
-    
-    var tmp, current, top = array.length;
-
-    if(top) while(--top) {
-        current = Math.floor(Math.random() * (top + 1));
-        tmp = array[current];
-        array[current] = array[top];
-        array[top] = tmp;
-    }
-
-    return array;
-  }
+	}
 }
-}
-wallPattern.init();
+
+document.body.style.background = 'url('+c.toDataURL()+')';
